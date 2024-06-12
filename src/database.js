4f261ed450dd713ead27f0ab7a63c6320ba5bc80
Juan Camussi <jjcamussi@gmail.com>
2024-06-11 23:04:31 -0300

    produccion

--- src/database.js
+++ src/database.js
@@ -3,12 +3,14 @@
 const mongoose = require ('mongoose') //importo el modulo mongoose, que me permite que el servidor interactue
 //con la base de datos
 
-mongoose.connect('mongodb://' + process.env.IP + 'candySystemDB') //realizo la conexion
+//const ip = '192.168.2.3'
+
+mongoose.connect('mongodb://' + process.env.IP + '/candySystemDB') //realizo la conexion
 
 const connection = mongoose.connection; //almaceno la conexion realizada, para trabajar con ella.
 
 connection.once('open', ()=>{    //una vez que la conexion este abierta...
 
-    console.log('El servidor se ha conectado a la base de datos: ', 'mongodb://localhost/candySystemDB')
+    console.log('El servidor se ha conectado a la base de datos! ')
     
 })
