b19c154c8b03a4035021ba81440e31eb28426187
Juan Camussi <jjcamussi@gmail.com>
2024-06-12 23:42:51 -0300

    produccion8

--- src/database.js
+++ src/database.js
@@ -5,8 +5,16 @@ const mongoose = require ('mongoose') //importo el modulo mongoose, que me permi
 
 const { MongoClient } = require("mongodb");
 
+//uri = 'mongodb://jjcamussi:A64wfQrByvjZ3rzz@ac-fdii898-shard-00-00.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-01.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-02.5otvwda.mongodb.net:27017/?ssl=true&replicaSet=atlas-qw3ysb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+//const client = new MongoClient(uri);
 
 const client = new MongoClient(process.env.URI);
+
+console.log(client)
+console.log(MongoClient)
+console.log(process.env.URI)
+console.log(typeof(process.env.URI))
+
 async function run() {
   try {
     await client.connect();
