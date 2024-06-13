5db1c088df46464055eb435bab3d2779c853bf2d
Juan Camussi <jjcamussi@gmail.com>
2024-06-13 00:31:43 -0300

    produccion12b

--- src/database.js
+++ src/database.js
@@ -5,20 +5,24 @@ const mongoose = require ('mongoose') //importo el modulo mongoose, que me permi
 
 const { MongoClient } = require("mongodb");
 
-//uri = 'mongodb://jjcamussi:A64wfQrByvjZ3rzz@ac-fdii898-shard-00-00.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-01.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-02.5otvwda.mongodb.net:27017/?ssl=true&replicaSet=atlas-qw3ysb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
-//const client = new MongoClient(uri);
+var url = 'mongodb://jjcamussi:A64wfQrByvjZ3rzz@ac-fdii898-shard-00-00.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-01.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-02.5otvwda.mongodb.net:27017/?ssl=true&replicaSet=atlas-qw3ysb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+const client = new MongoClient(url);
+
+//const client = new MongoClient(process.env.URL);
+
+
 
 
 async function run() {
 
-    const client = MongoClient.connect(process.env.URL)
+    await client.connect()
     const database = client.db("test");
     const ratings = database.collection("devices");
     
     const cursor = ratings.find();
     await cursor.forEach(doc => console.dir(doc));
 
-    console.log(database)
+  console.log(database)
 
 
 }
