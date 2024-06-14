09d4e8154c6c29a94eaaf9152c67ed22adf9fb01
Juan Camussi <jjcamussi@gmail.com>
2024-06-13 23:45:21 -0300

    produccion19

--- src/database.js
+++ src/database.js
@@ -3,32 +3,12 @@
 const mongoose = require ('mongoose') //importo el modulo mongoose, que me permite que el servidor interactue
 //con la base de datos
 
-//const { MongoClient } = require("mongodb");
-
-//var url = 'mongodb://jjcamussi:A64wfQrByvjZ3rzz@ac-fdii898-shard-00-00.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-01.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-02.5otvwda.mongodb.net:27017/?ssl=true&replicaSet=atlas-qw3ysb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
-
-/* var url = 'mongodb://localhost/candySystemDB'
-const client = new MongoClient(url); */
-
-//const client = new MongoClient(process.env.URL);
 
 
-
-
-/* async function run() {
-
-    await client.connect() */
-    //const database = client.db("test");
-    //const ratings = database.collection("devices");
+//var url = 'mongodb://jjcamussi:A64wfQrByvjZ3rzz@ac-fdii898-shard-00-00.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-01.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-02.5otvwda.mongodb.net:27017/candySystemDB?ssl=true&replicaSet=atlas-qw3ysb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
     
-/*     const cursor = ratings.find();
-    await cursor.forEach(doc => console.dir(doc));
-
-  console.log(database) */
 
 
-/* }
-run() */
 mongoose.connect(process.env.URL) //realizo la conexion
 
 const connection = mongoose.connection; //almaceno la conexion realizada, para trabajar con ella.
@@ -38,3 +18,4 @@ connection.once('open', ()=>{    //una vez que la conexion este abierta...
     console.log('El servidor se ha conectado a la base de datos!')
     
 })
+
