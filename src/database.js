30f319feb43be8c58d6cb0e4f71947b115206b35
Juan Camussi <jjcamussi@gmail.com>
2024-06-16 16:28:18 -0300

    produccion21

--- src/database.js
+++ src/database.js
@@ -5,8 +5,6 @@ const mongoose = require ('mongoose') //importo el modulo mongoose, que me permi
 
 
 
-//var url = 'mongodb://jjcamussi:A64wfQrByvjZ3rzz@ac-fdii898-shard-00-00.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-01.5otvwda.mongodb.net:27017,ac-fdii898-shard-00-02.5otvwda.mongodb.net:27017/candySystemDB?ssl=true&replicaSet=atlas-qw3ysb-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
-    
 
 
 mongoose.connect(process.env.URL) //realizo la conexion
